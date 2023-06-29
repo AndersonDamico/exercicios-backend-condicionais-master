4be346d37ba18f90261f86d10137556b9dab387b
AndersonDamico <andersondamico2@gmail.com>
2023-06-29 13:21:00 -0300

    resolução da questão 13

--- 13/index.js
+++ 13/index.js
@@ -1,6 +1,23 @@
 //tipo de pagamento (dinheiro, credito, debito, cheque).
-const tipoDePagamento = "credito";
+const tipoDePagamento = "dinheiro";
 
 //valor da mercadoria (centavos)
 const valorDoProduto = 13000;
 
+if (tipoDePagamento === "credito") {
+    const desconto = (valorDoProduto) *5/100;
+    const valorFinal = (valorDoProduto - desconto) /100;
+    console.log(`Valor a ser pago: R$${valorFinal.toFixed(2)}`)
+}else if (tipoDePagamento === "cheque") {
+    const desconto = (valorDoProduto) *3/100;
+    const valorFinal = (valorDoProduto - desconto) /100;
+    console.log(`Valor a ser pago: R$${valorFinal.toFixed(2)}`)
+}else if (tipoDePagamento === "debito") {
+    const desconto = (valorDoProduto) *10/100;
+    const valorFinal = (valorDoProduto - desconto) /100;
+    console.log(`Valor a ser pago: R$${valorFinal.toFixed(2)}`)
+}else if (tipoDePagamento === "dinheiro") {
+    const desconto = (valorDoProduto) *10/100;
+    const valorFinal = (valorDoProduto - desconto) /100;
+    console.log(`Valor a ser pago: R$${valorFinal.toFixed(2)}`)
+}
