14c8ced4ad0a7484120502ba378c16a060631e36
AndersonDamico <andersondamico2@gmail.com>
2023-06-29 14:29:20 -0300

    resolução da questão 17

--- 17/index.js
+++ 17/index.js
@@ -1,8 +1,15 @@
 //valor do produto comprado.
-const valorDoProduto = 100000;
+const valorDoProduto = 50000000;
 
 //quantidade de parcelas
 const quantidadeDoParcelamento = 10;
 
 //valor pago
 const valorPago = 300;
+
+if (valorDoProduto){
+    const valorParcela= (valorDoProduto /100) / quantidadeDoParcelamento;
+    const valorRestante= (valorDoProduto /100) - valorPago;
+    const parcelasRestantes = valorRestante / valorParcela;
+    console.log(`Restam ${parcelasRestantes.toFixed(0)} parcelas de R$${valorParcela.toFixed(2)}`)
+}
