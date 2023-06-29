c04812941a1b225b9ea47558dc399f68d60717e3
AndersonDamico <andersondamico2@gmail.com>
2023-06-29 12:50:10 -0300

    alteração da questão 11

--- 11/index.js
+++ 11/index.js
@@ -1,5 +1,5 @@
 //renda mensal do aluno, em centavos.
-const rendaMensalEmCentavos = 600000;
+const rendaMensalEmCentavos = 150000;
 
 // Tempo decorrido de contrato. Se for maior que 60 meses, o aluno não deve mais nada.
 const mesesDecorridos = 12;
@@ -9,7 +9,7 @@ const totalJaPagoPeloAluno = 100000;
 
 
 if (rendaMensalEmCentavos <= 200000) {
-    console.log("O valor da parcela desse mês é R$ 0 reais.")
+    console.log("O valor da parcela desse mês é R$ 0 reais. Nenhum valor é devido pois a renda do estudante está abaixo do valor mínimo de R$ 2000 reais.")
 }else if (mesesDecorridos >= 60){
     console.log("Após 60 meses,o valor da parcela é zero.")
 }else if (totalJaPagoPeloAluno >= 1800000) {
